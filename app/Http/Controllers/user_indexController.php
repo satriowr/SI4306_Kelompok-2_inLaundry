f628ea0103bcb7ac3f0658339cc11d2f1fda316c
satrio wicaksono <iam.satriorahman28@gmail.com>
2022-01-06 00:21:22 +0700

    create user_indexController

--- app/Http/Controllers/user_indexController.php
+++ app/Http/Controllers/user_indexController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class user_indexController extends Controller
 {
     public function index(){
-        return view('User.index');
+        return view('layouts.index-user');
     }
 }
