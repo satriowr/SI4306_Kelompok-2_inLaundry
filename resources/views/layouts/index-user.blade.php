b6d469e13196d56ce4228a2db2e60d69674ab813
satrio wicaksono <iam.satriorahman28@gmail.com>
2022-01-06 00:21:44 +0700

    Create Templating User

---
+++ resources/views/layouts/index-user.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+    <style>
+        .icon-navbar ion-icon{
+            font-size: 1.5em;
+            color: #1597E5;
+            opacity: 70%;
+        }
+    </style>
+    
+</head>
+<body>
+    <nav style="background-color:#F7FAFF" class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="/">
+              <img width="170" src="assets\logo\logo.png" alt="">
+          </a>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+
+          <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+            <div class="status-nav" style="width: 345px; height:48px; background-color:white; border-radius:100px; padding-top:12px; padding-left:5px">
+                <div class="icon-navbar">
+                    <a href="" style="text-decoration: none">
+                        <ion-icon name="cart" style="margin-left:12px"></ion-icon>
+                    </a>
+
+                    <a href="" style="text-decoration: none">
+                        <ion-icon name="notifications" style="margin-left:10px"></ion-icon>
+                    </a>
+                </div>
+            </div>
+          </div>
+        </div>
+    </nav>
+</body>
+</html>
