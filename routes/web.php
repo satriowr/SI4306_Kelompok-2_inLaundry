e7d3a4364e680d15fb1c49a6f4f7cce668d09594
wasikamin <wasikamin@student.telkomuniversity.ac.id>
2022-01-06 14:09:16 +0700

    Route Report page user

--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,6 @@ Route::get('/admin/report', function () {
     return view('Admin.report');
 });
 
-Route::get('/admin/product', function () {
-    return view('Admin.product');
-});
 
 Route::get('/signup', [registerController::class, 'index']);
 Route::post('/signup', [registerController::class, 'store']);
@@ -51,3 +48,7 @@ Route::get('/admin/products', [ProductsController::class, 'index']);
 Route::resource('/admin/products', ProductsController::class);
 
 Route::get('/home', [user_indexController::class, 'index']);
+
+Route::get('/report', function () {
+    return view('User.report');
+});
