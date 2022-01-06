a28d4c685670cb10e3ca19bcd2b2b81f6b1f66e5
wasikamin <wasikamin@student.telkomuniversity.ac.id>
2022-01-06 14:09:04 +0700

    Dummy Report Page user

--- resources/views/layouts/index-user.blade.php
+++ resources/views/layouts/index-user.blade.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    
 
     <style>
         .icon-navbar ion-icon{
@@ -87,7 +88,7 @@
         </div>
     </nav>
 
-    <div class="main">
+    <div class="container">
         @yield('container')
     </div>
     
