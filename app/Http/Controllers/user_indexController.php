882564c9d6a8cb9e4a6e73086c131ff9d2e160f2
satrio wicaksono <iam.satriorahman28@gmail.com>
2022-01-06 11:20:04 +0700

    Modify index function

--- app/Http/Controllers/user_indexController.php
+++ app/Http/Controllers/user_indexController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class user_indexController extends Controller
 {
     public function index(){
-        return view('layouts.index-user');
+        return view('User.index');
     }
 }
