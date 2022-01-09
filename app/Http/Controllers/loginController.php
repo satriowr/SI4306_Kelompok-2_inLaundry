8d9303526bdae0cd898a8851e6b35589557a5a71
wasikamin <wasikamin@student.telkomuniversity.ac.id>
2022-01-09 18:55:08 +0700

    update login controller

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -4,27 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 
 class loginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('Login.login');
     }
 
-    public function validates(Request $request){
+    public function validates(Request $request)
+    {
         $validate = $request->validate([
             'email' => 'required',
             'password' => 'required'
         ]);
 
 
-        if(Auth::attempt($validate)){
+        if (Auth::attempt($validate)) {
             $request->session()->regenerate();
             //dd("login iso");
             return redirect()->intended('home');
         }
-        
-        return back()->with('loginError', 'Email atau Password Salah');
 
+        return back()->with('loginError', 'Email atau Password Salah');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
     }
 }
