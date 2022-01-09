24b16f0ef06d04931b801e8ed349e20d0b27a42c
satrio wicaksono <iam.satriorahman28@gmail.com>
2022-01-10 00:31:13 +0700

    Create controller admin_dashboard

---
+++ app/Http/Controllers/admin_dashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\note;
+use DB;
+
+class admin_dashboardController extends Controller
+{
+    public function index(){
+        $showNote = note::all();
+        return view('Admin.dashboard', compact('showNote'));
+    }
+
+    public function deleteNote($id){
+        DB::table('notes')->where('id',$id)->delete();
+        $showNote = note::all();
+        return view('Admin.dashboard', compact('showNote'));
+    }
+
+    public function newNote(Request $request){
+        DB::table('notes')->insert([
+            'event' => $request->event,
+            'date' => $request->date,
+        ]);
+
+        return redirect('/admin/dashboard');
+    }
+}
