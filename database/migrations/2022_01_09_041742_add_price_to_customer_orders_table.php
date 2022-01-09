e33a427515c84a61bf2ac15ba6b88821c605d7c6
satrio wicaksono <iam.satriorahman28@gmail.com>
2022-01-09 11:21:45 +0700

    Create and Add migration table

--- database/migrations/2022_01_09_041742_add_price_to_customer_orders_table.php
+++ database/migrations/2022_01_09_041742_add_price_to_customer_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class AddPriceToCustomerOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,7 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('name');
+        Schema::table('customer_orders', function (Blueprint $table) {
             $table->integer('price');
         });
     }
@@ -28,6 +25,8 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::table('customer_orders', function (Blueprint $table) {
+            //
+        });
     }
 }
