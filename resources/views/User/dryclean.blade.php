7c75e7ad86d0d52239751c185ea5bd06215850c1
satrio wicaksono <iam.satriorahman28@gmail.com>
2022-01-09 11:21:02 +0700

    Create hidden input

--- resources/views/User/dryclean.blade.php
+++ resources/views/User/dryclean.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.index-user')
 
+
+
 @section('container')
     <div class="main-content" style="margin-top:100px; margin-left:100px;">
         <h2 style="font-weight:bold">Dry Clean</h2>
     </div>
-
-    
 
     <div class="form-fill d-flex flex-row gap-5" style="margin-left:100px; margin-top:50px; width:500px;">
         <div class="left">
@@ -18,10 +18,11 @@
 
                 <div class="mb-3">
                     <label for="geocoder" class="form-label">Customer Address</label>
-                    <div class="geocoder" id="geocoder">
-                        
-                    </div>
+                    <div class="geocoder" id="geocoder"></div>
                 </div>
+                <input type="hidden" name="service" value="Dry Clean">
+                <input type="hidden" name="weight" value="0">
+                <input type="hidden" name="price" value="0">
                 <button type="submit" class="btn btn-primary">Next</button>
 
             </form>
